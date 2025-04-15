35fedaae62f96fb0f6835275b14150b7c72f9662
MonkEHlam <118730168+MonkEHlam@users.noreply.github.com>
2025-04-15 17:43:34 +0700

    Implement models

--- CashFlow/CashFlowRecords/apps.py
+++ CashFlow/CashFlowRecords/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CashflowrecordsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CashFlowRecords'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "CashFlowRecords"
--- CashFlow/CashFlowRecords/models.py
+++ CashFlow/CashFlowRecords/models.py
@@ -1,3 +1,39 @@
+"""
+All entities which are stored in DB
+"""
+
 from django.db import models
 
-# Create your models here.
+
+MAXCHARINNAME = 40 # Just a simple constant
+
+
+class Category(models.Model):
+    """Represents the category of records."""
+    name = models.CharField(max_length=MAXCHARINNAME, unique=True)
+
+
+class Subcategory(models.Model):
+    """Represents the subcategory of records."""
+    name = models.CharField(max_length=MAXCHARINNAME, unique=True)
+    base_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Status(models.Model):
+    """Represents the status of records."""
+    name = models.CharField(max_length=MAXCHARINNAME, unique=True)
+
+
+class Type(models.Model):
+    """Represents the type of records."""
+    name = models.CharField(max_length=MAXCHARINNAME, unique=True)
+
+
+class CashFlowRecord(models.Model):
+    """Represents a cash flow record. Be careful, status, type, subcategory and comment fields can be null."""
+    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True)
+    amount = models.FloatField()
+    datestamp = models.fields.DateField()
+    comment = models.TextField(null=True)
